350e4007c52a67c3bbe4be3eefd847cd614efd95
Santiago Lorduy <lorduysantiago@gmail.com>
2025-01-31 17:56:08 -0500

    add prettier and ng-eslint, and path alias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,6 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: '<router-outlet />',
 })
-export class AppComponent {
-  title = 'planets';
-}
+export class AppComponent {}
