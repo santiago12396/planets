516d49153d41a5d73348661082b24555e46743da
Santiago Lorduy <lorduysantiago@gmail.com>
2025-01-31 19:12:07 -0500

    add environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  solarSystemApiUrl: 'https://api.le-systeme-solaire.net/rest',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  solarSystemApiUrl: 'https://api.le-systeme-solaire.net/rest',
+};
