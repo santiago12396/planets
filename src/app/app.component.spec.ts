a75e80d289e6bf9672fd6c846d22d227be10ed14
Santiago Lorduy <lorduysantiago@gmail.com>
2025-03-28 09:16:36 -0500

    tests in AppComponent

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+
+describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render "router-outlet"', () => {
+    expect(fixture.debugElement.query(By.css('router-outlet'))).toBeTruthy();
+  });
+});
